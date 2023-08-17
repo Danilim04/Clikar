816f521ec3ba395a7415ef095776e30e9b35a2e9
Daniel Alves Ferraz <daniel.ferraz04@hotmail.com>
2023-08-17 01:19:32 -0300

    responsive relese 2.2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,13 +16,11 @@ const HomePage = () => {
             <div className="containerhome">
                 <div className={`containerLogo ${menuOpen ? 'on' : ''}`}>
                     <img className={`logonav ${menuOpen ? 'logonavhidden' : ''}`} src="./images/logoBrancaJPG.jpg" alt="Logo clikar" />
-
-                    <div onClick={toggleMobileMenu} className='menu-toggle'>
-                        <div className='one'></div>
-                        <div className='two'></div>
-                        <div className='three'></div>
-                    </div>
-
+                        <div onClick={toggleMobileMenu} className={`menu-toggle ${menuOpen ? 'menu-toggleFullSize' : ''}`}>
+                            <div className='one'></div>
+                            <div className='two'></div>
+                            <div className='three'></div>
+                        </div>
                     <div className={`navitens-mobile ${menuOpen ? '' : 'navitens-mobilehidden'}`}>
                         <ul>
                             <a className="navbar-itens" href="">HOME</a>
@@ -32,28 +30,28 @@ const HomePage = () => {
                             <a className="navbar-itens" href="">CONTATO</a>
                         </ul>
                     </div>
-                    
-                <div className="navitens">
-                    <ul>
-                        <a className="navbar-itens" href="">Home</a>
-                        <a className="navbar-itens" href="">Sobre Nós</a>
-                        <a className="navbar-itens" href="">Serviços</a>
-                        <a className="navbar-itens" href="">Clientes</a>
-                        <a className="navbar-itens" href="">Contato</a>
-                    </ul>
-                </div>
+
+                    <div className="navitens">
+                        <ul>
+                            <a className="navbar-itens" href="">HOME</a>
+                            <a className="navbar-itens" href="">SOBRE NÓS</a>
+                            <a className="navbar-itens" href="">SERVIÇOS</a>
+                            <a className="navbar-itens" href="">CLIENTES</a>
+                            <a className="navbar-itens" href="">CONTATO</a>
+                        </ul>
+                    </div>
                 </div>
 
                 <div className="home" >
                     <div className='container-text-home' >
                         <div className='text-home' >
                             <h1>
-                                <span>OPTIMIZANDO</span> ELÉTRICAS<br /> CORPORATIVAS
+                                <span>OTIMIZANDO</span> ELÉTRICAS<br /> CORPORATIVAS
                             </h1>
                             <h2>
                                 ELETRICISTAS ESPECIALIZADOS <br /> EM <span>SOLUÇÕES CORPORATIVAS</span>
                             </h2>
-                            <button className='botao-home' > Faça seu Orçamento </button>
+                            <button className='botao-home' > FAÇA SEU ORÇAMENTO </button>
                         </div>
 
                     </div>
